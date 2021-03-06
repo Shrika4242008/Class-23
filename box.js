07302bbf40f0e93cf0b3ff31919602eae76fe2f8
Shrika4242008 <77925895+Shrika4242008@users.noreply.github.com>
2021-03-06 09:12:38 -0500

    Add files via upload

---
+++ box.js
@@ -0,0 +1,22 @@
+var box_options;
+
+class Box {
+constructor(x,y,width,height){
+  box_options = {
+    restitution: 1.0, friction: 0.8
+  }
+  this.body = Bodies.rectangle(x,y,width,height,box_options);
+  this.width = width;
+  this.height = height;
+  World .add(world, this.body);
+}
+display(){
+var angle = this.body.angle;
+push();
+translate(this.body.position.x,this.body.position.y);
+rotate(angle);
+rectMode(CENTER);
+rect(0,0,this.width,this.height);
+pop();
+}
+}
---
+++ sketch.js
@@ -0,0 +1,25 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,box2;
+var Ground1;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(200,120,30,30);
+    box2 = new Box(210,90,30,30);
+    Ground1 = new Ground(200,380,400,20);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box1.display();
+    box2.display();
+    Ground1.display();
+}
